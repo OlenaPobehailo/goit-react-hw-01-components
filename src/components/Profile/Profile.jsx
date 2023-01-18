b626c4960ef821b91ac208bbf59773870bb32cdc
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-01-18 04:17:32 +0200

    fix Profile
    Change class to className, formate document

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,27 @@
-export const Profile=({avatar, username,tag,location,stats})=>{
-    return(
-        <div class="profile">
-  <div class="description">
-    <img
-      src={avatar}
-      alt={username}
-      class="avatar"
-    />
-    <p class="name">{username}</p>
-    <p class="tag">{tag}</p>
-    <p class="location">{location}</p>
-  </div>
+export const Profile = ({ avatar, username, tag, location, stats }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt={username} className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{stats.followers}</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">{stats.views}</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{stats.likes}</span>
-    </li>
-  </ul>
-</div>
-    );
-}
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
