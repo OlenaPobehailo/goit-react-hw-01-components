3c8094629fa182a0dd617ee271f6bb872ee93d09
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-01-18 05:17:16 +0200

    add FriendList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
 import profile from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
 
 export const App = () => {
   return (
@@ -23,6 +25,7 @@ export const App = () => {
         stats={profile.stats}
       />
       <Statistics title={data.title} stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
