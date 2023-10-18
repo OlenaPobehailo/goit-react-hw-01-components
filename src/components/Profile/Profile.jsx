051cfba1d449b940922fb70d8fcc5be7cfe8d57d
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-10-18 18:48:22 +0300

    adds profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import css from './Profile.module.css';
+const Profile = ({
+  avatar,
+  username,
+  tag,
+  location,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>&#64;{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
