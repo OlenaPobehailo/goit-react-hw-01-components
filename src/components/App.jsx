b0b0a47db87e975e3e117f8803f4974599b8be73
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-10-18 20:20:46 +0300

    adds TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile';
 import Statistics from './Statistics';
 import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 
 import user from '../data/user.json';
 import stats from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -17,8 +19,10 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={stats} />
-
+      
       <FriendList friends={friends} />
+      
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
